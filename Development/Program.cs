5a88b8f946815b6dc0ea112c170498be145a81ba
Sadiq <samedov.sadiq55@gmail.com>
2023-11-12 02:19:36 +0400

    Add msg

--- Development/Program.cs
+++ Development/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello from branch - development and Ann!");
+
+            Console.WriteLine("Test");
         }
 
         public void Print(string text)
